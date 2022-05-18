feb686d6d484eae9b43c4af30494167c9204a24f
TymanWasTaken <tbeckman530@gmail.com>
2022-05-17 18:37:13 -0600

    No more token logging

---
+++ EncryptDMs/index.ts
@@ -0,0 +1,8 @@
+import { Plugin } from "aliucord/entities";
+import { ApplicationCommandOptionType } from "aliucord/api";
+
+export default class EncryptDMs extends Plugin {
+    public async start() {
+        // TODO figure out how the heck to get crypto in rn
+    }
+}
---
+++ Texter/index.ts
@@ -0,0 +1,8 @@
+import { Plugin } from "aliucord/entities";
+import { ApplicationCommandOptionType } from "aliucord/api";
+
+export default class Texter extends Plugin {
+    public async start() {
+        // TODO: implement plugin
+    }
+}
